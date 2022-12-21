7c30b793b68e20606996b965c20245959637f9a9
Angelita <angelitasvb@gmail.com>
2022-12-20 23:07:18 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title":"NOVO HOMEM DE FERRO EM 3D",
+        "description":"Marvel anuncia um novo homem de ferro.",
+        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQshUI-y6ECts6wfx2dmH7IRLlqQj9HdXH4ww&usqp=CAU",
+    }
+    ,{
+        "id":"2",
+        "title":"Nova série anunciada no Disney +",
+        "description":"bla bla bla",
+        "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK_X2TDh5wzqt7Hr0SwVPNxK0eWvSNIKIY9Q&usqp=CAU",
+    }
+]
